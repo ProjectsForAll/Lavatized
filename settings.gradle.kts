bad04b037e82626356430e5689375c586fc8d878
nitra <matt@plas.host>
2024-07-14 00:58:42 -0400

    Revert "Switch to using Fairy?"
    This reverts commit e15fb4e4

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-        maven("https://repo.imanity.dev/imanity-libraries")
-    }
-}
-
-rootProject.name = "JustPoints"
